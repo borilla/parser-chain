1a99e1c08eab5f4941fa24cddbe2a3a9917a70b0
borilla <github@borilla.co.uk>
2017-08-03 02:52:09 +0800

    Allow process method name to be configurable

--- processor-chain.js
+++ processor-chain.js
@@ -2,6 +2,7 @@ var ProcessorChain = (function() {
 
 	var ProcessorChain = function() {
 		this.reset();
+		this.processMethod = 'process';
 	}
 
 	var ProcessorChainPrototype = ProcessorChain.prototype;
@@ -20,9 +21,10 @@ var ProcessorChain = (function() {
 
 	ProcessorChainPrototype.process = function(msg) {
 		var processors = this.processors;
+		var processMethod = this.processMethod;
 		for (var i = 0, l = processors.length; i < l; ++i) {
 			var processor = processors[i];
-			var processed = processor.process(msg);
+			var processed = processor[processMethod](msg);
 			if (processed !== undefined) {
 				var callback = this.callbacks[i];
 				if (callback) {
